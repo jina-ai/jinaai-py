ab4e49fdbe4449ff7aade4c1554a4cf5474e0dca
Guillaume <47593967+redjson@users.noreply.github.com>
2023-06-21 14:00:39 +0800

    chore: standalone examples

---
+++ examples-standalone/ex2-fridge-recipe-generation.py
@@ -0,0 +1,44 @@
+from jinaai import JinaAI
+import os
+
+jinaai = JinaAI(
+    tokens = {
+        'promptperfect-token': os.environ.get('PROMPTPERFECT_TOKEN', ''),
+        'scenex-token': os.environ.get('SCENEX_TOKEN', ''),
+        'rationale-token': os.environ.get('RATIONALE_TOKEN', ''),
+        'chatcat-token': os.environ.get('CHATCAT_TOKEN', '')
+    }
+)
+
+def toBase64(img: str) -> str:
+    return jinaai.utils.image_to_base64(f"examples/images/{img}")
+
+fridge = toBase64('fridge-1.png')
+
+def generate():
+    try:
+        # 1. get a description of the fridge content
+        descriptions = jinaai.describe(
+            fridge,
+            { 'question': 'What ingredients are in the fridge?', 'languages': ['en'] }
+        )
+        print('DESCRIPTION:\n', descriptions['results'][0]['output'])
+        # 2. get an optmised prompt
+        prompt = jinaai.optimize('\n'.join([
+            'Give me one recipe based on this list for ingredients',
+            *['INGREDIENTS:\n' + desc['output'] for i, desc in enumerate(descriptions['results'])]
+        ]))
+        print('PROMPT:\n', prompt['results'][0]['output'])
+        # 3. get a recipe based on the descriptions
+        recipe = jinaai.generate(prompt['results'][0]['output'])
+        print('RECIPE:\n', recipe['output'])
+        # 4. get a swot analysis of the recommendation
+        swot = jinaai.decide(
+            recipe['output'],
+            { 'analysis': 'swot' }
+        )
+        print('SWOT:\n', swot['results'][0]['swot'])
+    except Exception as e:
+        print("Error:", str(e))
+
+generate()
---
+++ examples-standalone/ex3-movie-tweets-analysis.py
@@ -0,0 +1,53 @@
+from jinaai import JinaAI
+import os
+
+jinaai = JinaAI(
+    tokens = {
+        'promptperfect-token': os.environ.get('PROMPTPERFECT_TOKEN', ''),
+        'scenex-token': os.environ.get('SCENEX_TOKEN', ''),
+        'rationale-token': os.environ.get('RATIONALE_TOKEN', ''),
+        'chatcat-token': os.environ.get('CHATCAT_TOKEN', '')
+    }
+)
+
+positiveMovieTweets = [
+    'Just watched the new movie! The plot was incredible, and the visual effects were mind-blowing. Definitely a must-see! #movie #amazing',
+    "I can't stop thinking about the movie. The acting was superb, and the twist at the end caught me off guard. Highly recommended! #movie #thriller",
+    'The cinematography in the movie was stunning. Every scene was like a work of art. #movie #cinema',
+    'The new movie is a rollercoaster of emotions. I laughed, I cried, and I was on the edge of my seat throughout the entire film. #movie #emotional',
+    "Just came back from watching the movie, and I'm still speechless. It's a masterpiece! #movie #masterpiece",
+    "If you're looking for a good movie to watch, I highly recommend this one. It has a compelling story and brilliant performances. #movie #recommendation",
+    'The movie exceeded my expectations. The pacing was perfect, and the characters were so well-developed. #movie #surprise',
+    "I'm still trying to process what I just witnessed in the movie. It's unlike anything I've ever seen before. #movie #unique",
+    "Can't get enough of the movie's soundtrack. It perfectly complements the visuals and adds so much depth to the film. #movie #soundtrack",
+    "Just watched the movie with my friends, and we had a blast. It's entertaining from start to finish. #movie #fun"
+]
+
+negativeMovieTweets = [
+    'I just watched the new movie, and it was a complete disappointment. The plot was confusing, and the acting was terrible. #movie #disappointed',
+    'Save your money and skip this movie. It was boring and predictable. #movie #boring',
+    "I don't understand the hype around this movie. It was overrated and not worth the ticket price. #movie #overrated",
+    'I had high expectations for this movie, but it fell flat. The storyline was weak, and the characters were poorly developed. #movie #letdown',
+    'I regret watching this movie. It was a waste of time. #movie #wasteoftime',
+    "I can't believe I paid to see this movie. It was absolutely awful. #movie #awful",
+    'The movie was a disaster. The dialogue was cringe-worthy, and the special effects were laughable. #movie #disaster',
+    'I was bored throughout the entire movie. It lacked any excitement or originality. #movie #uninteresting',
+    'I was really looking forward to this movie, but it was a major letdown. The pacing was off, and the ending was unsatisfying. #movie #majorletdown',
+    "I don't recommend this movie at all. It was a total mess and didn't make any sense. #movie #notrecommended"
+]
+
+def evaluate(tweets):
+    try:
+        # 1. get the general feeling according to the tweets
+        prompt = '\n'.join([
+            'According to those tweets, is the general feeling positive or negative?',
+            'Reply by [POSITIVE] or [NEGATIVE]',
+            *['TWEET:\n' + t for i, t in enumerate(tweets)]
+        ])
+        feeling = jinaai.generate(prompt)
+        print('GENERAL FEELING:', feeling['output'])
+    except Exception as e:
+        print("Error:", str(e))
+
+evaluate(positiveMovieTweets)
+evaluate(negativeMovieTweets)
