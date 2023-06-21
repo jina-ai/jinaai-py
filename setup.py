a6d33467399bd40e1097bddec33957ec361d959a
Guillaume <47593967+redjson@users.noreply.github.com>
2023-06-21 13:49:29 +0800

    chore: update version

--- setup.py
+++ setup.py
@@ -1,12 +1,18 @@
 from setuptools import setup, find_packages
+import pathlib
+
+HERE = pathlib.Path(__file__).parent
+README = (HERE / "README.md").read_text()
 
 setup(
     name='jinaai',
-    version='0.1.1',
+    version='0.1.2',
     author='Jina AI',
     author_email='guillaume.roncari@jina.ai',
     description='Jina AI Python SDK',
     url='https://github.com/jina-ai/jinaai-py.git',
     packages=find_packages(),
     install_requires=[],
+    long_description=README,
+    long_description_content_type='text/markdown', 
 )
