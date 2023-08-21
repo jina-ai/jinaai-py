19f57e911a17b10f9864b93d37af1ec2e5b4edc2
Guillaume <47593967+redjson@users.noreply.github.com>
2023-08-21 12:31:27 +0800

    chore: version bump

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name='jinaai',
-    version='0.2.5',
+    version='0.2.6',
     author='Jina AI',
     author_email='guillaume.roncari@jina.ai',
     description='Jina AI Python SDK',
